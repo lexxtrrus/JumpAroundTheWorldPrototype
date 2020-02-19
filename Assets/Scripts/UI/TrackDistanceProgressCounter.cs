8cce5daa89104823e71558f13101cf60b2fd97df
topografsusanin <36396971+topografsusanin@users.noreply.github.com>
2020-02-19 04:07:22 +0300

    world map rotation correct

--- Assets/Scripts/UI/TrackDistanceProgressCounter.cs
+++ Assets/Scripts/UI/TrackDistanceProgressCounter.cs
@@ -17,11 +17,47 @@ public class TrackDistanceProgressCounter : MonoBehaviour
     
     private void LateUpdate() 
     {
-        Quaternion rot = Quaternion.FromToRotation(_planet.up, _player.up);
-        rot.z = rot.x;
-        rot.x = 0f;
-        _worldImage.rotation = Quaternion.Euler(0f, 0f, rot.eulerAngles.z); 
-        _totalDegrees = Mathf.RoundToInt((rot.eulerAngles.z * 100f) / 360f);
+        if(_player.position.y > 0 && _player.position.z >= 0)
+        {
+            Quaternion rot = Quaternion.FromToRotation(_planet.up, _player.up);
+            rot.z = rot.x;
+            rot.x = 0f;
+
+            _worldImage.rotation = Quaternion.Euler(0f, 0f, rot.eulerAngles.z);
+            _totalDegrees = Mathf.RoundToInt((rot.eulerAngles.z * 100f) / 360f);
+            //Debug.Log(_worldImage.localRotation.z);
+        }
+        else if(_player.position.y <= 0 && _player.position.z > 0)
+        {
+            Quaternion rot = Quaternion.FromToRotation(_planet.forward, _player.up);
+            rot.z = rot.x;
+            rot.x = 0f;
+
+            _worldImage.rotation = Quaternion.Euler(0f, 0f, 90f + rot.eulerAngles.z);
+            _totalDegrees = Mathf.RoundToInt((rot.eulerAngles.z + 90f) * 100f / 360f);
+            //Debug.Log(_worldImage.localRotation.z);
+        }
+        else if(_player.position.y <= 0 && _player.position.z <= 0)
+        {
+            Quaternion rot = Quaternion.FromToRotation(-1f * _planet.up, _player.up);
+            rot.z = rot.x;
+            rot.x = 0f;
+
+            _worldImage.rotation = Quaternion.Euler(0f, 0f, 180f + rot.eulerAngles.z);
+            _totalDegrees = Mathf.RoundToInt((rot.eulerAngles.z + 180f) * 100f / 360f);
+            //Debug.Log(_worldImage.localRotation.z);
+        }
+        else if(_player.position.y >= 0 && _player.position.z < 0)
+        {
+            Quaternion rot = Quaternion.FromToRotation(-1f * _planet.forward, _player.up);
+            rot.z = rot.x;
+            rot.x = 0f;
+
+            _worldImage.rotation = Quaternion.Euler(0f, 0f, 270f + rot.eulerAngles.z);
+            _totalDegrees = Mathf.RoundToInt((rot.eulerAngles.z + 270f) * 100f / 360f);
+            //Debug.Log(_worldImage.localRotation.z);
+        }
+        
         _distanceCounterText.text = $"{_totalDegrees.ToString()}%";
         //Debug.Log(rot.eulerAngles.z);
     }
